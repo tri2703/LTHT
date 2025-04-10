c924a061860fa93114f6c8e54ad053ea09c0dc0d
vinh9873 <nguyenvinh9837@gmail.com>
2025-04-10 10:34:58 +0700

    Add authorization

--- client.c
+++ client.c
@@ -11,24 +11,22 @@
 
 #define LENGTH 2048
 
-// Global variables
 volatile sig_atomic_t flag = 0;
 int sockfd = 0;
 char name[32];
 
-void str_overwrite_stdout() {
-  printf("%s", "> ");
-  fflush(stdout);
+void str_trim_lf (char* arr, int length) {
+    for (int i = 0; i < length; i++) {
+        if (arr[i] == '\n') {
+            arr[i] = '\0';
+            break;
+        }
+    }
 }
 
-void str_trim_lf (char* arr, int length) {
-  int i;
-  for (i = 0; i < length; i++) { // trim \n
-    if (arr[i] == '\n') {
-      arr[i] = '\0';
-      break;
-    }
-  }
+void str_overwrite_stdout() {
+    printf("> ");
+    fflush(stdout);
 }
 
 void catch_ctrl_c_and_exit(int sig) {
@@ -36,105 +34,113 @@ void catch_ctrl_c_and_exit(int sig) {
 }
 
 void send_msg_handler() {
-  char message[LENGTH] = {};
-	char buffer[LENGTH + 32] = {};
+    char message[LENGTH] = {};
+    char buffer[LENGTH + 32] = {};
 
-  while(1) {
-  	str_overwrite_stdout();
-    fgets(message, LENGTH, stdin);
-    str_trim_lf(message, LENGTH);
+    while(1) {
+        str_overwrite_stdout();
+        fgets(message, LENGTH, stdin);
+        str_trim_lf(message, LENGTH);
 
-    if (strcmp(message, "exit") == 0) {
-			break;
-    } else {
-      sprintf(buffer, "%s: %s\n", name, message);
-      send(sockfd, buffer, strlen(buffer), 0);
+        if (strcmp(message, "exit") == 0) {
+            break;
+        } else {
+            sprintf(buffer, "%s: %s\n", name, message);
+            send(sockfd, buffer, strlen(buffer), 0);
+        }
+
+        bzero(message, LENGTH);
+        bzero(buffer, LENGTH + 32);
     }
-
-		bzero(message, LENGTH);
-    bzero(buffer, LENGTH + 32);
-  }
-  catch_ctrl_c_and_exit(2);
+    catch_ctrl_c_and_exit(2);
 }
 
 void recv_msg_handler() {
-	char message[LENGTH] = {};
-  while (1) {
-		int receive = recv(sockfd, message, LENGTH, 0);
-    if (receive > 0) {
-      printf("%s", message);
-      str_overwrite_stdout();
-    } else if (receive == 0) {
-			break;
-    } else {
-			// -1
-		}
-		memset(message, 0, sizeof(message));
-  }
+    char message[LENGTH] = {};
+    while (1) {
+        int receive = recv(sockfd, message, LENGTH, 0);
+        if (receive > 0) {
+            printf("%s", message);
+            str_overwrite_stdout();
+        } else if (receive == 0) {
+            break;
+        }
+        memset(message, 0, sizeof(message));
+    }
 }
 
 int main(int argc, char **argv){
-	if(argc != 2){
-		printf("Usage: %s <port>\n", argv[0]);
-		return EXIT_FAILURE;
-	}
-
-	char *ip = "127.0.0.1";
-	int port = atoi(argv[1]);
-
-	signal(SIGINT, catch_ctrl_c_and_exit);
-
-	printf("Please enter your name: ");
-  fgets(name, 32, stdin);
-  str_trim_lf(name, strlen(name));
-
-
-	if (strlen(name) > 32 || strlen(name) < 2){
-		printf("Name must be less than 30 and more than 2 characters.\n");
-		return EXIT_FAILURE;
-	}
-
-	struct sockaddr_in server_addr;
-
-	/* Socket settings */
-	sockfd = socket(AF_INET, SOCK_STREAM, 0);
-  server_addr.sin_family = AF_INET;
-  server_addr.sin_addr.s_addr = inet_addr(ip);
-  server_addr.sin_port = htons(port);
-
-
-  // Connect to Server
-  int err = connect(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr));
-  if (err == -1) {
-		printf("ERROR: connect\n");
-		return EXIT_FAILURE;
-	}
-
-	// Send name
-	send(sockfd, name, 32, 0);
-
-	printf("=== WELCOME TO THE CHATROOM ===\n");
-
-	pthread_t send_msg_thread;
-  if(pthread_create(&send_msg_thread, NULL, (void *) send_msg_handler, NULL) != 0){
-		printf("ERROR: pthread\n");
-    return EXIT_FAILURE;
-	}
-
-	pthread_t recv_msg_thread;
-  if(pthread_create(&recv_msg_thread, NULL, (void *) recv_msg_handler, NULL) != 0){
-		printf("ERROR: pthread\n");
-		return EXIT_FAILURE;
-	}
-
-	while (1){
-		if(flag){
-			printf("\nBye\n");
-			break;
+    if(argc != 3){
+        printf("Usage: %s <server_ip> <port>\n", argv[0]);
+        return EXIT_FAILURE;
     }
-	}
 
-	close(sockfd);
+    char *ip = argv[1];
+    int port = atoi(argv[2]);
 
-	return EXIT_SUCCESS;
+    signal(SIGINT, catch_ctrl_c_and_exit);
+
+    printf("Do you want to [login] or [register]? ");
+    char action[10];
+    fgets(action, sizeof(action), stdin);
+    str_trim_lf(action, sizeof(action));
+
+    printf("Username: ");
+    fgets(name, 32, stdin);
+    str_trim_lf(name, 32);
+
+    char password[32];
+    printf("Password: ");
+    fgets(password, 32, stdin);
+    str_trim_lf(password, 32);
+
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    struct sockaddr_in server_addr;
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = inet_addr(ip);
+    server_addr.sin_port = htons(port);
+
+    int err = connect(sockfd, (struct sockaddr*)&server_addr, sizeof(server_addr));
+    if (err == -1) {
+        printf("ERROR: connect\n");
+        return EXIT_FAILURE;
+    }
+
+    char auth_message[100];
+    sprintf(auth_message, "%s|%s|%s", action, name, password);
+    send(sockfd, auth_message, strlen(auth_message), 0);
+
+    char auth_response[100];
+    int bytes = recv(sockfd, auth_response, sizeof(auth_response), 0);
+    auth_response[bytes] = '\0';
+    printf("%s\n", auth_response);
+
+    if (strcmp(auth_response, "OK") != 0) {
+        close(sockfd);
+        return EXIT_FAILURE;
+    }
+
+    printf("=== WELCOME TO THE CHATROOM ===\n");
+
+    pthread_t send_msg_thread;
+    if(pthread_create(&send_msg_thread, NULL, (void *) send_msg_handler, NULL) != 0){
+        printf("ERROR: pthread\n");
+        return EXIT_FAILURE;
+    }
+
+    pthread_t recv_msg_thread;
+    if(pthread_create(&recv_msg_thread, NULL, (void *) recv_msg_handler, NULL) != 0){
+        printf("ERROR: pthread\n");
+        return EXIT_FAILURE;
+    }
+
+    while (1){
+        if(flag){
+            printf("\nBye\n");
+            break;
+        }
+    }
+
+    close(sockfd);
+    return EXIT_SUCCESS;
 }
--- server.c
+++ server.c
@@ -16,215 +16,230 @@
 static _Atomic unsigned int cli_count = 0;
 static int uid = 10;
 
-/* Client structure */
-typedef struct{
-	struct sockaddr_in address;
-	int sockfd;
-	int uid;
-	char name[32];
+typedef struct {
+    struct sockaddr_in address;
+    int sockfd;
+    int uid;
+    char name[32];
 } client_t;
 
 client_t *clients[MAX_CLIENTS];
-
 pthread_mutex_t clients_mutex = PTHREAD_MUTEX_INITIALIZER;
 
-void str_overwrite_stdout() {
-    printf("\r%s", "> ");
-    fflush(stdout);
-}
-
-void str_trim_lf (char* arr, int length) {
-  int i;
-  for (i = 0; i < length; i++) { // trim \n
-    if (arr[i] == '\n') {
-      arr[i] = '\0';
-      break;
+void str_trim_lf(char* arr, int length) {
+    for (int i = 0; i < length; i++) {
+        if (arr[i] == '\n') {
+            arr[i] = '\0';
+            break;
+        }
     }
-  }
 }
 
-void print_client_addr(struct sockaddr_in addr){
-    printf("%d.%d.%d.%d",
-        addr.sin_addr.s_addr & 0xff,
-        (addr.sin_addr.s_addr & 0xff00) >> 8,
-        (addr.sin_addr.s_addr & 0xff0000) >> 16,
-        (addr.sin_addr.s_addr & 0xff000000) >> 24);
+int check_credentials(const char* username, const char* password) {
+    FILE *fp = fopen("accounts.txt", "r");
+    if (!fp) return 0;
+    char u[32], p[32];
+    while (fscanf(fp, "%s %s", u, p) != EOF) {
+        if (strcmp(u, username) == 0 && strcmp(p, password) == 0) {
+            fclose(fp);
+            return 1;
+        }
+    }
+    fclose(fp);
+    return 0;
 }
 
-/* Add clients to queue */
+int register_user(const char* username, const char* password) {
+    FILE *fp_check = fopen("accounts.txt", "r");
+    if (fp_check) {
+        char u[32], p[32];
+        while (fscanf(fp_check, "%s %s", u, p) != EOF) {
+            if (strcmp(u, username) == 0) {
+                fclose(fp_check);
+                return 0;
+            }
+        }
+        fclose(fp_check);
+    }
+
+    FILE *fp = fopen("accounts.txt", "a");
+    if (!fp) return 0;
+    fprintf(fp, "%s %s\n", username, password);
+    fclose(fp);
+    return 1;
+}
+
 void queue_add(client_t *cl){
-	pthread_mutex_lock(&clients_mutex);
-
-	for(int i=0; i < MAX_CLIENTS; ++i){
-		if(!clients[i]){
-			clients[i] = cl;
-			break;
-		}
-	}
-
-	pthread_mutex_unlock(&clients_mutex);
+    pthread_mutex_lock(&clients_mutex);
+    for(int i=0; i < MAX_CLIENTS; ++i){
+        if(!clients[i]){
+            clients[i] = cl;
+            break;
+        }
+    }
+    pthread_mutex_unlock(&clients_mutex);
 }
 
-/* Remove clients to queue */
 void queue_remove(int uid){
-	pthread_mutex_lock(&clients_mutex);
-
-	for(int i=0; i < MAX_CLIENTS; ++i){
-		if(clients[i]){
-			if(clients[i]->uid == uid){
-				clients[i] = NULL;
-				break;
-			}
-		}
-	}
-
-	pthread_mutex_unlock(&clients_mutex);
+    pthread_mutex_lock(&clients_mutex);
+    for(int i=0; i < MAX_CLIENTS; ++i){
+        if(clients[i]){
+            if(clients[i]->uid == uid){
+                clients[i] = NULL;
+                break;
+            }
+        }
+    }
+    pthread_mutex_unlock(&clients_mutex);
 }
 
-/* Send message to all clients except sender */
 void send_message(char *s, int uid){
-	pthread_mutex_lock(&clients_mutex);
-
-	for(int i=0; i<MAX_CLIENTS; ++i){
-		if(clients[i]){
-			if(clients[i]->uid != uid){
-				if(write(clients[i]->sockfd, s, strlen(s)) < 0){
-					perror("ERROR: write to descriptor failed");
-					break;
-				}
-			}
-		}
-	}
-
-	pthread_mutex_unlock(&clients_mutex);
+    pthread_mutex_lock(&clients_mutex);
+    for(int i=0; i<MAX_CLIENTS; ++i){
+        if(clients[i]){
+            if(clients[i]->uid != uid){
+                if(write(clients[i]->sockfd, s, strlen(s)) < 0){
+                    perror("ERROR: write to descriptor failed");
+                    break;
+                }
+            }
+        }
+    }
+    pthread_mutex_unlock(&clients_mutex);
 }
 
-/* Handle all communication with the client */
 void *handle_client(void *arg){
-	char buff_out[BUFFER_SZ];
-	char name[32];
-	int leave_flag = 0;
+    char buff_out[BUFFER_SZ];
+    int leave_flag = 0;
 
-	cli_count++;
-	client_t *cli = (client_t *)arg;
+    cli_count++;
+    client_t *cli = (client_t *)arg;
 
-	// Name
-	if(recv(cli->sockfd, name, 32, 0) <= 0 || strlen(name) <  2 || strlen(name) >= 32-1){
-		printf("Didn't enter the name.\n");
-		leave_flag = 1;
-	} else{
-		strcpy(cli->name, name);
-		sprintf(buff_out, "%s has joined\n", cli->name);
-		printf("%s", buff_out);
-		send_message(buff_out, cli->uid);
-	}
+    char buffer[100];
+    if (recv(cli->sockfd, buffer, sizeof(buffer), 0) <= 0) {
+        leave_flag = 1;
+    } else {
+        // Parse: action|username|password
+        char *action = strtok(buffer, "|");
+        char *username = strtok(NULL, "|");
+        char *password = strtok(NULL, "|");
 
-	bzero(buff_out, BUFFER_SZ);
+        if (!action || !username || !password) {
+            send(cli->sockfd, "Invalid format", 14, 0);
+            leave_flag = 1;
+        } else if (strcmp(action, "login") == 0) {
+            if (check_credentials(username, password)) {
+                strcpy(cli->name, username);
+                send(cli->sockfd, "OK", 2, 0);
+                sprintf(buff_out, "%s has joined\n", cli->name);
+                printf("%s", buff_out);
+                send_message(buff_out, cli->uid);
+            } else {
+                send(cli->sockfd, "Login failed", 13, 0);
+                leave_flag = 1;
+            }
+        } else if (strcmp(action, "register") == 0) {
+            if (register_user(username, password)) {
+                strcpy(cli->name, username);
+                send(cli->sockfd, "OK", 2, 0);
+                sprintf(buff_out, "%s has registered and joined\n", cli->name);
+                printf("%s", buff_out);
+                send_message(buff_out, cli->uid);
+            } else {
+                send(cli->sockfd, "User already exists", 20, 0);
+                leave_flag = 1;
+            }
+        } else {
+            send(cli->sockfd, "Unknown action", 14, 0);
+            leave_flag = 1;
+        }
+    }
 
-	while(1){
-		if (leave_flag) {
-			break;
-		}
+    bzero(buff_out, BUFFER_SZ);
 
-		int receive = recv(cli->sockfd, buff_out, BUFFER_SZ, 0);
-		if (receive > 0){
-			if(strlen(buff_out) > 0){
-				send_message(buff_out, cli->uid);
+    while(1){
+        if (leave_flag) break;
 
-				str_trim_lf(buff_out, strlen(buff_out));
-				printf("%s -> %s\n", buff_out, cli->name);
-			}
-		} else if (receive == 0 || strcmp(buff_out, "exit") == 0){
-			sprintf(buff_out, "%s has left\n", cli->name);
-			printf("%s", buff_out);
-			send_message(buff_out, cli->uid);
-			leave_flag = 1;
-		} else {
-			printf("ERROR: -1\n");
-			leave_flag = 1;
-		}
+        int receive = recv(cli->sockfd, buff_out, BUFFER_SZ, 0);
+        if (receive > 0){
+            if(strlen(buff_out) > 0){
+                send_message(buff_out, cli->uid);
+                str_trim_lf(buff_out, strlen(buff_out));
+                printf("%s -> %s\n", buff_out, cli->name);
+            }
+        } else {
+            sprintf(buff_out, "%s has left\n", cli->name);
+            printf("%s", buff_out);
+            send_message(buff_out, cli->uid);
+            leave_flag = 1;
+        }
+        bzero(buff_out, BUFFER_SZ);
+    }
 
-		bzero(buff_out, BUFFER_SZ);
-	}
+    close(cli->sockfd);
+    queue_remove(cli->uid);
+    free(cli);
+    cli_count--;
+    pthread_detach(pthread_self());
 
-  /* Delete client from queue and yield thread */
-	close(cli->sockfd);
-  queue_remove(cli->uid);
-  free(cli);
-  cli_count--;
-  pthread_detach(pthread_self());
-
-	return NULL;
+    return NULL;
 }
 
 int main(int argc, char **argv){
-	if(argc != 2){
-		printf("Usage: %s <port>\n", argv[0]);
-		return EXIT_FAILURE;
-	}
+    if(argc != 2){
+        printf("Usage: %s <port>\n", argv[0]);
+        return EXIT_FAILURE;
+    }
 
-	char *ip = "127.0.0.1";
-	int port = atoi(argv[1]);
-	int option = 1;
-	int listenfd = 0, connfd = 0;
-  struct sockaddr_in serv_addr;
-  struct sockaddr_in cli_addr;
-  pthread_t tid;
+    char *ip = "0.0.0.0"; // cho phép mọi client kết nối
+    int port = atoi(argv[1]);
+    int option = 1;
+    int listenfd = 0, connfd = 0;
+    struct sockaddr_in serv_addr, cli_addr;
+    pthread_t tid;
 
-  /* Socket settings */
-  listenfd = socket(AF_INET, SOCK_STREAM, 0);
-  serv_addr.sin_family = AF_INET;
-  serv_addr.sin_addr.s_addr = inet_addr(ip);
-  serv_addr.sin_port = htons(port);
+    signal(SIGPIPE, SIG_IGN);
+    listenfd = socket(AF_INET, SOCK_STREAM, 0);
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = inet_addr(ip);
+    serv_addr.sin_port = htons(port);
 
-  /* Ignore pipe signals */
-	signal(SIGPIPE, SIG_IGN);
+    if(setsockopt(listenfd, SOL_SOCKET,(SO_REUSEPORT | SO_REUSEADDR), (char*)&option, sizeof(option)) < 0){
+        perror("ERROR: setsockopt failed");
+        return EXIT_FAILURE;
+    }
 
-	if(setsockopt(listenfd, SOL_SOCKET,(SO_REUSEPORT | SO_REUSEADDR),(char*)&option,sizeof(option)) < 0){
-		perror("ERROR: setsockopt failed");
-    return EXIT_FAILURE;
-	}
+    if(bind(listenfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0) {
+        perror("ERROR: Socket binding failed");
+        return EXIT_FAILURE;
+    }
 
-	/* Bind */
-  if(bind(listenfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0) {
-    perror("ERROR: Socket binding failed");
-    return EXIT_FAILURE;
-  }
+    if (listen(listenfd, 10) < 0) {
+        perror("ERROR: Socket listening failed");
+        return EXIT_FAILURE;
+    }
 
-  /* Listen */
-  if (listen(listenfd, 10) < 0) {
-    perror("ERROR: Socket listening failed");
-    return EXIT_FAILURE;
-	}
+    printf("=== CHATROOM SERVER STARTED ===\n");
 
-	printf("=== WELCOME TO THE CHATROOM ===\n");
+    while(1){
+        socklen_t clilen = sizeof(cli_addr);
+        connfd = accept(listenfd, (struct sockaddr*)&cli_addr, &clilen);
 
-	while(1){
-		socklen_t clilen = sizeof(cli_addr);
-		connfd = accept(listenfd, (struct sockaddr*)&cli_addr, &clilen);
+        if((cli_count + 1) == MAX_CLIENTS){
+            printf("Max clients reached. Rejecting...\n");
+            close(connfd);
+            continue;
+        }
 
-		/* Check if max clients is reached */
-		if((cli_count + 1) == MAX_CLIENTS){
-			printf("Max clients reached. Rejected: ");
-			print_client_addr(cli_addr);
-			printf(":%d\n", cli_addr.sin_port);
-			close(connfd);
-			continue;
-		}
+        client_t *cli = (client_t *)malloc(sizeof(client_t));
+        cli->address = cli_addr;
+        cli->sockfd = connfd;
+        cli->uid = uid++;
 
-		/* Client settings */
-		client_t *cli = (client_t *)malloc(sizeof(client_t));
-		cli->address = cli_addr;
-		cli->sockfd = connfd;
-		cli->uid = uid++;
+        queue_add(cli);
+        pthread_create(&tid, NULL, &handle_client, (void*)cli);
+        sleep(1);
+    }
 
-		/* Add client to the queue and fork thread */
-		queue_add(cli);
-		pthread_create(&tid, NULL, &handle_client, (void*)cli);
-
-		/* Reduce CPU usage */
-		sleep(1);
-	}
-
-	return EXIT_SUCCESS;
+    return EXIT_SUCCESS;
 }
